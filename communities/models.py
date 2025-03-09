2934d3fb89cf3993984c19b0a9f9a10361524409
Ireal <pesotskygoscha@yandex.ru>
2025-03-09 15:45:09 +0300

    feat: added community roles system

--- communities/models.py
+++ communities/models.py
@@ -1,23 +1,41 @@
-from sqlalchemy import Column, Integer, String, Table, ForeignKey
+from enum import Enum as PyEnum
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 
 from settings import Base
 
-user_community = Table(
-    'user_community', Base.metadata,
-    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
-    Column('community_id', Integer, ForeignKey('community.id'), primary_key=True)
-)
+
+class CommunityRoleEnum(PyEnum):
+    admin = "admin"
+    moderator = "moderator"
+    user = "user"
+
+
+class CommunityMembership(Base):
+    __tablename__ = "community_membership"
+
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    community_id = Column(Integer, ForeignKey("community.id"), primary_key=True)
+    role = Column(Enum(CommunityRoleEnum), nullable=False, default=CommunityRoleEnum.user)
+
+    user = relationship("User", back_populates="community_memberships")
+    community = relationship("Community", back_populates="community_memberships")
 
 
 class Community(Base):
     __tablename__ = "community"
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String, nullable=True)
 
     creator_id = Column(Integer, ForeignKey("user.id"), nullable=False)
 
-    users = relationship("User", secondary=user_community, back_populates="communities")
+    community_memberships = relationship(
+        "CommunityMembership",
+        back_populates="community",
+        cascade="all, delete-orphan"
+    )
     posts = relationship("Post", back_populates="communities")
     creator = relationship("User", back_populates="created_communities", foreign_keys=[creator_id])
